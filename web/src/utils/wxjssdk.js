18eb0aa6a4ffe0fe3dc9afabda83c23d1fdc3cc3
wenjiexin <13609053257@163.com>
2021-01-12 18:17:48 +0800

    修改微信分享

--- web/src/utils/wxjssdk.js
+++ web/src/utils/wxjssdk.js
@@ -35,107 +35,66 @@ export function wxjsconfig(to) {
 				jsApiList: config.jsApiList// 必填，需要使用的JS接口列表
 			});
 			wx.ready(function () {
-				_share(to)
+				share(to)
 			});
 
 		}
 	})
 }
 
-
-// 批量隐藏功能按钮接口
-export function wxHideMenuItems() {
-	wx.ready(function () {
-		// config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
-		wx.hideMenuItems({
-			menuList: ["menuItem:share:appMessage", "menuItem:share:timeline", "menuItem:share:qq", "menuItem:share:weiboApp", "menuItem:favorite", "menuItem:share:facebook", "menuItem:share:QZone"] // 要隐藏的菜单项，只能隐藏“传播类”和“保护类”按钮，所有menu项见附录3
-		});
+function wxShare(option) {
+	const {shareTitle, shareLink, shareImage, shareDesc} = option
+	wx.updateTimelineShareData({
+		title: shareTitle, // 分享标题
+		link: shareLink, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+		imgUrl: shareImage, // 分享图标
+		success: function (res) {
+			// 设置成功
+		}
 	});
+	// 发送给好友
+	wx.updateAppMessageShareData({
+		title: shareTitle, // 分享标题
+		link: shareLink, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+		imgUrl: shareImage, // 分享图标
+		desc: shareDesc,
+		success: function (res) {
+			// 设置成功
+		}
+	});
+	// 发送到tx微博
+	wx.onMenuShareWeibo({
+		title: shareTitle, // 分享标题
+		link: shareLink, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+		imgUrl: shareImage, // 分享图标
+		desc: shareDesc,
+		success: function (res) {
+			// 设置成功
+		}
+	})
 }
 
-function _share(to) {
-	let goodsImage = "";
-	let goodsName = "";
+function share(to) {
+	const option = {
+		shareTitle:  "likeShop",
+		shareLink: window.location.origin,
+		shareImage:  "",
+		shareDesc: "likeShop分销电商系统"
+	}
 	if (to.name == "goodsDetail") {
 		getGoodsDetail({ id: to.query.id }).then(res => {
 			if (res.code == 1) {
-				goodsImage = res.data.image;
-				goodsName = res.data.name;
-				const shareTitle = "likeShop";
-				const shareLink = window.location.origin + to.fullPath;
-				const shareImage = goodsImage;
-				const shareDesc = goodsName;
+				const {image , name} = res.data
+				option.shareLink = window.location.href
+				option.shareImage = image;
+				option.shareDesc = name;
 				// 朋友圈 QQ空间
-				wx.updateTimelineShareData({
-					title: shareTitle, // 分享标题
-					link: shareLink, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-					imgUrl: shareImage, // 分享图标
-					desc: shareDesc,
-					success: function (res) {
-						// 设置成功
-					}
-				});
-				// 发送给好友
-				wx.updateAppMessageShareData({
-					title: shareTitle, // 分享标题
-					link: shareLink, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-					imgUrl: shareImage, // 分享图标
-					desc: shareDesc,
-					success: function (res) {
-						// 设置成功
-					}
-				});
-				// 发送到tx微博
-				wx.onMenuShareWeibo({
-					title: shareTitle, // 分享标题
-					link: shareLink, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-					imgUrl: shareImage, // 分享图标
-					desc: shareDesc,
-					success: function (res) {
-						// 设置成功
-					}
-				})
+				wxShare(option)
 			}
 		})
 		return;
 	}
-	else {
-		const shareTitle = "likeShop";
-		const shareLink = to.name == "goodsDetail" ? window.location.origin + to.fullPath : window.location.origin;
-		const shareImage = goodsImage ? goodsName : "";
-		const shareDesc = goodsName ? goodsName : "";
-
-		// 朋友圈 QQ空间
-		wx.updateTimelineShareData({
-			title: shareTitle, // 分享标题
-			link: shareLink, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-			imgUrl: shareImage, // 分享图标
-			desc: shareDesc,
-			success: function (res) {
-				// 设置成功
-			}
-		});
-		// 发送给好友
-		wx.updateAppMessageShareData({
-			title: shareTitle, // 分享标题
-			link: shareLink, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-			imgUrl: shareImage, // 分享图标
-			desc: shareDesc,
-			success: function (res) {
-				// 设置成功
-			}
-		});
-		// 发送到tx微博
-		wx.onMenuShareWeibo({
-			title: shareTitle, // 分享标题
-			link: shareLink, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-			imgUrl: shareImage, // 分享图标
-			desc: shareDesc,
-			success: function (res) {
-				// 设置成功
-			}
-		})
-	}
+	wxShare(option)
 }
 
 export function wxPay(opt) {

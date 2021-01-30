e7a6ce59cebf31c69c25182d58241a1a6f33c14b
wenjiexin <13609053257@163.com>
2021-01-30 14:24:06 +0800

    修复bug

--- uniapp/App.vue
+++ uniapp/App.vue
@@ -10,10 +10,7 @@
 			// #ifdef H5
 			uni.getSystemInfo({
 				success(e) {
-					/* 窗口宽度大于420px且不在PC页面且不在移动设备时跳转至 PC.html 页面 */
 					if (e.windowWidth > 420 && !window.top.isPC && !/iOS|Android/i.test(e.system)) {
-						// window.location.pathname = 'https://java.crmeb.net/';
-						/* 若你的项目未设置根目录（默认为 / 时），则使用下方代码 */
 						window.location.pathname = '/static/html/pc.html';
 					}
 				}

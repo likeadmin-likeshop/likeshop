bc69441ac96466d2e12a020a2efa735cc4bd4c12
caijianhao <227371644@qq.com>
2021-02-01 11:16:36 +0800

    修改短信默认开启状态

--- server/config/project.php
+++ server/config/project.php
@@ -1,6 +1,6 @@
 <?php
 return [
     'file_domain' => \think\facade\Env::get('project.file_domain', 'likeshop.yixiangonline.com'),
-    'sms' => \think\facade\Env::get('project.sms', false),
+    'sms' => \think\facade\Env::get('project.sms', true),
     'version'=>'2.0.2.20210107',
 ];

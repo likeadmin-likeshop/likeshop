5585a0c1deea65f46276fce99d1636bed98d73e7
octopus <s969980719@vip.qq.com>
2021-02-03 11:29:05 +0800

    优化信息

--- uniapp/config/app.js
+++ uniapp/config/app.js
@@ -2,5 +2,5 @@ module.exports = {
     HEADER: {
         'content-type': 'application/json'
     },
-    baseURL: 'https://bkh.yixiangonline.com/api/', // appid： wxfb9759f93884b6d1
+    baseURL: 'http://likeshop.yixiangonline.com/api/',
 }

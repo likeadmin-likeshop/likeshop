8d9c142f1bab9cd7d3d2841b276bd2b5f0d524f9
octopus3 <s969980719@vip.qq.com>
2021-02-08 14:18:40 +0800

    uniapp打包版本

--- uniapp/config/app.js
+++ uniapp/config/app.js
@@ -2,5 +2,10 @@ module.exports = {
     HEADER: {
         'content-type': 'application/json'
     },
-    baseURL: 'http://likeshopv2.yixiangonline.com/api/',
+	// #ifdef H5
+    baseURL: process.env.NODE_ENV == 'production' ? '/api/' : 'http://likeshopv2.yixiangonline.com/api/', // appid： wxfb9759f93884b6d1
+	// #endif
+	// #ifdef MP-WEIXIN
+	baseURL: 'http://likeshopv2.yixiangonline.com/api/'
+	// #endif
 }

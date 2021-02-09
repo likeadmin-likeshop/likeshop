3c1241baa263309a77d2f3260a1a87dc16b72d84
octopus3 <s969980719@vip.qq.com>
2021-02-09 12:43:36 +0800

    地址修复

--- uniapp/config/app.js
+++ uniapp/config/app.js
@@ -3,7 +3,7 @@ module.exports = {
         'content-type': 'application/json'
     },
 	// #ifdef H5
-    baseURL: process.env.NODE_ENV == 'production' ? '/api/' : 'http://likeshopv2.yixiangonline.com/api/', // appid： wxfb9759f93884b6d1
+    baseURL: process.env.NODE_ENV == 'production' ? '/api/' : 'http://likeshopv2.yixiangonline.com/api/',
 	// #endif
 	// #ifdef MP-WEIXIN
 	baseURL: 'http://likeshopv2.yixiangonline.com/api/'

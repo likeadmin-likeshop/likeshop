13d961bf64a7c2c8e70b9957b5e801a6362aaa2d
lr <>
2021-02-11 19:51:18 +0800

    发布版本

--- uniapp/config/cachekey.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-	USER_INFO: 'USER_INFO',
-	TOKEN: 'TOKEN',
-	BACK_URL: 'BACK_URL',
-	LOGIN_NUM: 'LOGIN_NUM'
-}
--- uniapp/store/getters.js
+++
@@ -1,7 +0,0 @@
-export default {
-  userInfo: state => state.app.userInfo || {},
-  token: state => state.app.token,
-  isLogin: state => !!state.app.token,
-  cartNum: state => state.app.cartNum,
-  loginNum: state => state.app.loginNum
-};

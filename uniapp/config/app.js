d4498ee9a98ce961e62a5b9f686044104f23d1f1
octopus <s969980719@vip.qq.com>
2021-02-22 16:34:54 +0800

    文件打包

--- uniapp/config/app.js
+++ uniapp/config/app.js
@@ -3,7 +3,7 @@ module.exports = {
         'content-type': 'application/json'
     },
 	// #ifdef H5
-    baseURL: process.env.NODE_ENV == 'production' ? '/api/' : 'http://likeshopv2.yixiangonline.com/api/',
+    baseURL: process.env.NODE_ENV == 'production' ? '/api/' : '/api/',
 	// #endif
 	// #ifdef MP-WEIXIN
 	baseURL: 'http://likeshopv2.yixiangonline.com/api/'

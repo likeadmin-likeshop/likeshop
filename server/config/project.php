9520bd0113a3b70dc25cf13a8f9ba68698b44f65
lr <>
2021-03-26 17:11:32 +0800

    调整版本号

--- server/config/project.php
+++ server/config/project.php
@@ -2,6 +2,6 @@
 return [
     'file_domain' => \think\facade\Env::get('project.file_domain', 'likeshop.yixiangonline.com'),
     'sms' => \think\facade\Env::get('project.sms', true),
-    'version'=>'2.1.1.20210228',
-    'front_version' => '2.1.1.20210228',
+    'version'=>'2.3.0.20210326',
+    'front_version' => '2.3.0.20210326',
 ];
